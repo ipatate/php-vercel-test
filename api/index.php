c8caaeeb76488e9ba2e3f37420f8459ec7a7b4ee
Patrick Faramaz <faramazpat@gmail.com>
2020-12-11 16:19:00 +0100

    add form

--- api/index.php
+++ api/index.php
@@ -1,4 +1,10 @@
 <?php
 
-header('content-type: application/json');
-echo json_encode(['time' => time(), 'date' => date('d.m.Y'), 'tech' => 'Vercel']);
+// header('content-type: application/json');
+// echo json_encode(['time' => time(), 'date' => date('d.m.Y'), 'tech' => 'Vercel']);
+
+echo '<h2>votre message:' . $_POST['message'] . '</h2>';
+
+echo '<script>
+setTimeout(function(){window.location.href = \'/\'}, 3000);
+</script>';
